2a4b2f381b31bcf3c48489a0a8213ae5191aba59
VictoriaParedes9 <vparedessobrero@escuelasproa.edu.ar>
2024-11-06 11:27:52 -0300

    Add files via upload

---
+++ 6noviembre.py
@@ -0,0 +1,4 @@
+persona={"nombreCompleto":("Victoria","PParedes"),"Edad": 14,"Direccion":"Avellaneda 421","Actividades favoritas":["Jugar basquet","Jugar video juegos","Comer"]}
+
+for c,v in persona.items():
+    print(c,"=",v)
